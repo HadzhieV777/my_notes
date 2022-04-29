121013b6603a5cb949280c93679968e8c1ee6e51
Radoslav Hadzhiev <bdwhadjiev42@gmail.com>
2022-04-29 09:03:12 +0300

    Edited notes list view

--- my_notes/main/urls.py
+++ my_notes/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from main.views import NotesListAndCreateView, CategoryListView
+from main.views import NotesListAndCreateView, CategoryListView, NoteDetailsAndUpdateView
 
 urlpatterns = (
     path('', NotesListAndCreateView.as_view(), name='notes list api'),
-    path('categories/', CategoryListView.as_view(), name='categories list api')
+    path('categories/', CategoryListView.as_view(), name='categories list api'),
+    path('/<int:pk>/', NoteDetailsAndUpdateView.as_view(), name='note details or update api'),
 )
